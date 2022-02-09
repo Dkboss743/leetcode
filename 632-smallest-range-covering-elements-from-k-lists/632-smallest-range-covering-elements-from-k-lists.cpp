e7d4af11e9a9627ae58acfa66b1cb83c0144f56d
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2022-02-09 21:53:43 +0530

    Time: 73 ms (57.39%), Space: 14.4 MB (87.16%) - LeetHub

--- 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
@@ -9,22 +9,25 @@ public:
   priority_queue<pii, vector<pii>,greater<pii>> data; // MIN HEAP it will be paired with the index of array it is in.
       
   int max = INT_MIN;
-  for (int i = 0; i < n; i++) {  
-       if (nums[i][0] > max)
-             max = nums[i][0];    // tracking local max to compare range
-             data.push(make_pair(nums[i][0], i));
-           }
+  for (int i = 0; i < n; i++) 
+  {  
+       if (nums[i][0] > max)        max = nums[i][0];       // tracking local max to compare range
+       data.push(make_pair(nums[i][0], i));
+  }
   int range = max - data.top().first;
   vector<int>val = {data.top().first, max};
   while (true) {                
-    int indo = data.top().second;
-    int ind = ++index[indo];
-    if (ind >=nums[indo].size())   // if it reaches end of array break
-      break;
-    if (nums[indo][ind] > max )
-      max =nums[indo][ind];                
+    int parent_index = data.top().second;
+    int sub_index = ++index[parent_index];
+    if (sub_index >=nums[parent_index].size()) 
+        break;  // if it reaches end of array break 
+      
+    if (nums[parent_index][sub_index] > max )
+      max =nums[parent_index][sub_index];  
+     
     data.pop();
-    data.push(make_pair(nums[indo][ind], indo));
+    data.push(make_pair(nums[parent_index][sub_index], parent_index));
+    
     int min = data.top().first;
     if (max - min < range) {
       val[0] = min;
@@ -32,7 +35,7 @@ public:
       range = max - min;
     }
   }
-        return val;
+return val;
     
     }
 };
