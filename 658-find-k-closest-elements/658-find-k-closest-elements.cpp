5b990aa60de892762a9bebb6acf2c2547139b3fb
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2022-02-09 16:10:15 +0530

    Time: 40 ms (84.22%), Space: 31.8 MB (40.11%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
- priority_queue<pair<int,int>> data;
-  for (int i = 0; i < k; i++)
-    data.push(make_pair(abs(arr[i] - x), arr[i]));
-  for (int i = k; i < arr.size(); i++) {
-    if (abs(arr[i] - x) < data.top().first ||
-        (abs(arr[i] - x) == data.top().first &&
-         (arr[i] < data.top().second))) {
-      data.pop();
-      data.push(make_pair(abs(arr[i] - x), arr[i]));
-    }
-  }
-  vector<int> ans(k);
-  for (int i = 0; i < k; i++) {
-    ans[i] = data.top().second;
-    data.pop();
-  }
-  sort(ans.begin(), ans.end());
-  return ans;
+     int i = 0; 
+     int j = arr.size()-1;
+     while(j-i>=k){
+         if(abs(arr[i] - x) > abs(arr[j] - x))
+             i++;
+         else
+             j--;
+     }
+     vector<int> ans;
+        for(int a = i ; a<=j ; a++)
+            ans.push_back(arr[a]);
+     return ans;
     }
 };
