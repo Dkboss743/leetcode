2a69bd2e1aa361eeed7b33f55635621bdf1f4a04
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2022-02-09 21:49:20 +0530

    Time: 93 ms (41.39%), Space: 14.4 MB (96.02%) - LeetHub

--- 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,25 +1,28 @@
+#define pii pair<int,int>
 class Solution {
 public:
-    vector<int> smallestRange(vector<vector<int>>& nums) {
-        int n = nums.size();
-  vector<int> index(n, 0);
-  priority_queue<pair<int, int>, vector<pair<int, int>>,
-                 greater<pair<int, int>>> data;
+  
+  vector<int> smallestRange(vector<vector<int>>& nums) {
+  int n = nums.size();
+  vector<int> index(n, 0);  // for traversing the minimum element array
+      
+  priority_queue<pii, vector<pii>,greater<pii>> data; // MIN HEAP it will be paired with the index of array it is in.
+      
   int max = INT_MIN;
-  for (int i = 0; i < n; i++) {
-    if (nums[i][0] > max)
-      max = nums[i][0];
-    data.push(make_pair(nums[i][0], i));
-  }
+  for (int i = 0; i < n; i++) {  
+       if (nums[i][0] > max)
+             max = nums[i][0];    // tracking local max to compare range
+             data.push(make_pair(nums[i][0], i));
+           }
   int range = max - data.top().first;
   vector<int>val = {data.top().first, max};
-  while (1) {
+  while (true) {                
     int indo = data.top().second;
     int ind = ++index[indo];
-    if (ind >=nums[indo].size())
+    if (ind >=nums[indo].size())   // if it reaches end of array break
       break;
     if (nums[indo][ind] > max )
-      max =nums[indo][ind];
+      max =nums[indo][ind];                
     data.pop();
     data.push(make_pair(nums[indo][ind], indo));
     int min = data.top().first;
