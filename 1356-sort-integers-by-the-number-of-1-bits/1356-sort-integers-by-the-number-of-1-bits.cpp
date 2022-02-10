e4d582b62fd29fa2ef600f43c7b10aa88eec9573
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2022-02-10 14:22:46 +0530

    Time: 17 ms (36.68%), Space: 10 MB (85.84%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,27 @@
+  bool compare(int a,int b){
+    int count1 = 0;
+    int count2= 0;
+    int val1 = a;
+    int val2 = b;
+    while(a>0){
+        a = (a&(a-1));
+        count1++;
+    }
+    while(b>0){
+        b = (b&(b-1));
+        count2++;
+    }
+    if(count1 == count2)
+    return val1<val2;
+    else
+    return count1<count2;
+ 
+}
+class Solution {
+public:
+
+vector<int> sortByBits(vector<int> &arr) {
+    sort(arr.begin(),arr.end(),compare);
+    return arr;      
+}
+};
