857586b1c22d5af9f799adb9dac684753298d2b1
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2022-02-11 20:58:18 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (58.40%) - LeetHub

--- 861-score-after-flipping-matrix/861-score-after-flipping-matrix.cpp
+++ 861-score-after-flipping-matrix/861-score-after-flipping-matrix.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:   
     int matrixScore(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int ans = (1<<(m-1))*n;
-        for(int col=1 ; col<m; col++){
+        int height = grid.size();
+        int width = grid[0].size();
+        int ans = (1<<(width-1))*height;  /* [1000]
+                                             [1000]
+                                             [1000]   */  
+/*if [row,col] == [row,0] which means they will be 1 so we can swap if they are less than non equal ones*/
+        for(int col=1 ; col<width; col++){
             int flips = 0;
-             for(int row=0; row<n; row++){
+             for(int row=0; row<height; row++){
                  if(grid[row][col] == grid[row][0])
                      flips++;
              }  
-            ans+=max(flips,n-flips)*(1<<(m-1-col));
+            ans+=max(flips,height-flips)*(1<<(width-1-col));
         }
         return ans;
         
