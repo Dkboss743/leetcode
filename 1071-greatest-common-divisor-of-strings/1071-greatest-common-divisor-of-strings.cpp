7f4cdd1b42cfc461772413984db301cd692527bc
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2022-03-15 15:28:38 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (64.84%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -3,10 +3,7 @@ public:
     string gcdOfStrings(string str1, string str2) {
         if(str1+str2!=str2+str1)
             return "";
-        int m = str1.length();
-        int n = str2.length();
-        int g = gcd(m,n);
-        return str1.substr(0,g);
+        return str1.substr(0,gcd(str1.length(),str2.length()));
        
     }
 };
