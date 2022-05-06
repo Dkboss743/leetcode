842bccdd2563bb18af4a802187346c82c1608443
Monu <66054595+Dkboss743@users.noreply.github.com>
2022-05-06 21:47:36 +0530

    Time: 25 ms (21.49%), Space: 17.1 MB (64.88%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -3,30 +3,25 @@ public:
     int maxDistToClosest(vector<int>& seats) {
         int ans = -1;
         int val = 0;
-        bool check_end = false;
-        bool check_start = false;
+        bool temp = true;
         int n = seats.size();
         for(int i=0; i<n ; i++){
             val = 0;
             while(i<n && seats[i] == 0){
-                if(i == 0)
-                    check_start = true;
                 val++;
-                if(i==n-1)
-                    check_end = true;
                 i++;
             }
-            if(val >=ans && check_end){
-                return val;
+            if(temp && seats[0] == 0){
+                ans = val;
+                temp = false;
             }
-            else if(val >=ans && check_start){
+            if(i == n && seats[n-1] == 0 && val>=ans){
                 ans = val;
             }
             else{
             val = (val+1)/2;  
             ans = max(ans,val);
             }
-            check_start = false;
         }
         return ans;
     }
