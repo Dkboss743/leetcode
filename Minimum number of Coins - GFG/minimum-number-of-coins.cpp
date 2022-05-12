5a85c46a0be612eee58131ba4b1fac3d904a296a
Monu <66054595+Dkboss743@users.noreply.github.com>
2022-05-12 11:09:54 +0530

    Added solution - LeetHub

--- Minimum number of Coins - GFG/minimum-number-of-coins.cpp
+++ Minimum number of Coins - GFG/minimum-number-of-coins.cpp
@@ -9,14 +9,34 @@ using namespace std;
 
 class Solution{
 public:
+    int binSearch(vector<int> coins, int val){
+        int n = coins.size();
+        int low = 0;
+        int high = n-1;
+        int ans = 0;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(coins[mid] == val){
+                return coins[mid];
+            }
+            else if(coins[mid] > val){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+                ans = coins[mid];
+            }
+        }
+        return ans;
+    }
     vector<int> minPartition(int N)
     {
         vector<int> coins = { 1, 2, 5, 10, 20, 50, 100, 200, 500, 2000 };
         vector<int> ans;
         while(N){
-            auto it = upper_bound(coins.begin(),coins.end(),N);
-            N -= *(it-1);
-            ans.push_back(*(it-1));
+            int val = binSearch(coins,N);
+            N -= val;
+            ans.push_back(val);
         }
         return ans;
     }
