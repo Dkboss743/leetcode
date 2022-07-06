d812d079529b56fd85e0056146de2e3e92c3f9b9
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2022-07-06 10:55:12 +0530

    Added solution - LeetHub

---
+++ Nth node from end of linked list - GFG/nth-node-from-end-of-linked-list.c
@@ -0,0 +1,91 @@
+// { Driver Code Starts
+//Initial Template for C
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+}*start;
+
+void insert();
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+//Function to find the data of nth node from the end of a linked list.
+int getNthFromLast(struct Node *head, int n)
+{
+    int p = 0;
+    struct Node *temp = head;
+    while(temp){
+        temp = temp->next;
+        p++;
+    }
+    n = p-n+1;
+    int ans = -1;
+    while(head){
+        n--;
+        if(n == 0){
+            ans = head->data;
+        }
+        head = head->next;
+    }
+    return ans;
+}
+
+// { Driver Code Starts.
+
+
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+      start=NULL;
+      int n,k;
+      scanf("%d",&n);
+      scanf("%d",&k);
+      insert(n);
+      int res = getNthFromLast(start,k);
+      printf("%d\n",res);
+    }
+    return 0;
+
+}
+
+
+ void insert(int n)
+ {   int value,i;
+     struct Node *temp;
+     for(i=0;i<n;i++)
+     {
+         scanf("%d",&value);
+         if(i==0)
+         {
+              start=(struct Node *) malloc( sizeof(struct Node) );
+              start->data=value;
+              start->next=NULL;
+              temp=start;
+              continue;
+         }
+         else
+         {
+             temp->next= (struct Node *) malloc( sizeof(struct Node) );
+             temp=temp->next;
+             temp->data=value;
+             temp->next=NULL;
+         }
+     }
+ }
+ 
+
+
+
+
+  // } Driver Code Ends
