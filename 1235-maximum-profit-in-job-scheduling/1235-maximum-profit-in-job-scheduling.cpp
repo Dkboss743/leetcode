f9264966a1d93b1b766e7c0f942582fd8234b2fc
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2022-08-06 14:51:15 +0530

    Time: 262 ms (51.74%), Space: 59.4 MB (48.63%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    vector<int> dp;
-    int util(vector<int> &startTime, vector<vector<int>> &jobs , int index){
-      int n  = startTime.size();
-      if( index >= n ){
-          return  0;
-      }
-      if(dp[index]!=-1){
-          return dp[index];
-      }
-      int a = util(startTime, jobs , index+1);
-      int next = lower_bound(startTime.begin() , startTime.end() , jobs[index][1]) - startTime.begin();
-      int b = util(startTime , jobs , next) + jobs[index][2];
-      return dp[index] = max(a , b);
-    }
+   
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         vector<vector<int>> jobs;
         int n = startTime.size();
-        for(int i=0; i<n ;i++){
-            jobs.push_back({startTime[i], endTime[i] , profit[i]});
+        for(int i=0; i<n; i++){
+            jobs.push_back({endTime[i],startTime[i],profit[i]});
         }
-        sort(jobs.begin() , jobs.end());
-        sort(startTime.begin(), startTime.end());
-        dp = vector<int>(n,-1);
-        return util(startTime, jobs, 0);
+        sort(jobs.begin(),jobs.end());
+        vector<int>dp(n);
+        dp[0] = jobs[0][2];
+        for(int i=1; i<n ;i++){
+            int f = 0;
+            for(int j = i-1; j>=0; j--){
+                if(jobs[i][1] >= jobs[j][0]){
+                    f= dp[j];
+                    break;
+                }
+            }
+            dp[i] = max(dp[i-1],f+jobs[i][2]);
+        }
+        return dp[n-1];
         
     }
 };
