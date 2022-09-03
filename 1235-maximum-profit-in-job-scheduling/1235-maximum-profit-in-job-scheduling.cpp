45de25639570027d4012458118b05b2b55dbb549
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2022-09-03 17:18:06 +0530

    Time: 179 ms (85.29%), Space: 59 MB (58.62%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
+  
    
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         vector<vector<int>> jobs;
         int n = startTime.size();
         for(int i=0; i<n; i++){
-            jobs.push_back({endTime[i],startTime[i],profit[i]});
+            jobs.push_back({endTime[i] , startTime[i] , profit[i]});
         }
-        sort(jobs.begin(),jobs.end());
-        vector<int>dp(n);
+        sort(jobs.begin(), jobs.end());
+        int dp[n];
         dp[0] = jobs[0][2];
-        for(int i=1; i<n ;i++){
-            int f = 0;
-            for(int j = i-1; j>=0; j--){
+        for(int i=1; i<n; i++){
+            int temp = 0;
+            for(int j=i-1; j>=0; j--){
                 if(jobs[i][1] >= jobs[j][0]){
-                    f= dp[j];
+                    temp = dp[j];
                     break;
                 }
             }
-            dp[i] = max(dp[i-1],f+jobs[i][2]);
+            dp[i] = max(dp[i-1], temp+jobs[i][2]);
         }
         return dp[n-1];
         
