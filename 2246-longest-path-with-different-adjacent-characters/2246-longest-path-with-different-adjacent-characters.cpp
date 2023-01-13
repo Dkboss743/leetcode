e53ae784f0f8472c9e66fbc03623418a73f48531
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2023-01-13 17:15:17 +0530

    Time: 607 ms (69.44%), Space: 187.9 MB (53.30%) - LeetHub

---
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> gr;
+    int ans;
+    int dfs(string &s , int idx , int par){
+        int curr = 0;
+        for(int i = 0;i <gr[idx].size() ; i++){
+            if(gr[idx][i] != par){
+                int temp = dfs(s, gr[idx][i] , idx);
+                if(temp != -1){
+                    ans = max(ans , curr + temp + 1);
+                    curr = max(curr , temp);
+                }
+            }
+        }
+        if(par != -1 && s[idx] != s[par]){
+            return curr+1;
+        }
+        return -1;
+    }
+    int longestPath(vector<int>& parent, string s) {
+        ans = 1;
+        gr.resize(s.size());
+        if(parent.size() == 1){
+            return 1;
+        }
+        for(int i = 1; i<parent.size() ; i++){
+            gr[parent[i]].push_back(i);
+            gr[i].push_back(parent[i]);
+        }
+        dfs(s, 0 , -1);
+        return ans;
+    }
+    
+};
