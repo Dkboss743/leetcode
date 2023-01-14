8f5d7d38439ab1ab75f1e9e31b53e1a56ebcf68d
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2023-01-14 21:06:51 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (96.89%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -4,9 +4,8 @@ public:
         int n = s.size();
         int ans = 0;
         for(int i = 0; i<n-2; i++){
-           if(s[i] !=s[i+1] && s[i] != s[i+2] && s[i+1] != s[i+2]){
-               ans++;
-           }
+           if(s[i] ==s[i+1] || s[i] == s[i+2] || s[i+1] == s[i+2]) continue;
+           else ans++;
         }
         return ans;
     }
