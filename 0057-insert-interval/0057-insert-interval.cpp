4ae491df1c001ebfa282db0d4a883502b16d13ec
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2023-01-16 17:57:43 +0530

    Time: 16 ms (83.48%), Space: 17.1 MB (45.20%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -14,18 +14,6 @@ public:
             if(intervals[i][1] < st){
                 ans.push_back(intervals[i]);
             }
-            else if(intervals[i][0]>st && intervals[i][0] >=en){
-                
-                if(intervals[i][0] == en){
-                    ans.push_back({st  , intervals[i][1]});
-                }
-                else{
-                    ans.push_back({st , en});
-                    ans.push_back(intervals[i]);
-                    
-                }
-                st = INT_MAX;
-            }
             else{
                 int j = i;
                 int s = min(st , intervals[i][0]);
