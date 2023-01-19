476df375312b7009142d6a2ca6b0e8779da879b7
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2023-01-19 19:27:17 +0530

    Time: 9 ms (29.29%), Space: 7.3 MB (17.47%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
@@ -3,14 +3,37 @@ public:
 
     // Encodes a URL to a shortened URL.
     string ans;
+    unordered_map<string,string> mp;
+    int len = 6;
+    string hash(string l){
+        string hashed = "";
+        for(int i = 0; i<6; i++){
+        int val = rand()%3;
+        if(val == 0){
+            hashed += to_string(rand()%10);
+        }
+        else if(val == 1){
+            hashed += ('a' + rand()%26);
+        }
+        else{
+            hashed += ('A' + rand()%26);
+        }
+        }
+        if(mp.find(hashed)!=mp.end()){
+            return hash(l);
+        }
+        mp[hashed] = l;
+        return hashed;
+    }
     string encode(string longUrl) {
-        this->ans = longUrl;
-        return ans;
+         string t = "http://tinyurl.com/";
+        cout << hash(longUrl) << endl;
+        return t + hash(longUrl);
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return ans;
+        return mp[shortUrl.substr(shortUrl.size() - len, len)];
     }
 };
 
