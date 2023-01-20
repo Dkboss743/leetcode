d021bc32330b36a5b876a7906fc2c57c7c0d1017
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2023-01-20 20:34:51 +0530

    Time: 111 ms (42.53%), Space: 28 MB (36.95%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     set<vector<int>> ans;
     void util(vector<int> &nums, int i , vector<int> &temp){
-        if(temp.size() > 1){
-            ans.insert(temp);
+        if(temp.size() > 2){
+            ans.insert(vector(temp.begin() + 1 , temp.end()));
         }
         for(i; i<nums.size() ; i++){
-            if(temp.size() == 0 || nums[i] >= temp[temp.size() - 1]){
+            if(nums[i] >= temp[temp.size() - 1]){
                 temp.push_back(nums[i]);
                 util(nums , i+1, temp);
                 temp.pop_back();
@@ -14,7 +14,7 @@ public:
         }
     }
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        vector<int> temp;
+        vector<int> temp = {-108};
         util(nums , 0 , temp);
         vector<vector<int>> val;
         for(auto x : ans){
