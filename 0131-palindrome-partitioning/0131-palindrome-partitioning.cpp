3b5d9fd480a56033b73dc61e7a27ca1c36acdf4f
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2023-01-22 21:33:37 +0530

    Time: 283 ms (25.37%), Space: 167.3 MB (9.96%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    bool pal(string s){
+        string t = s;
+        reverse(s.begin() , s.end());
+        return t==s;
+    }
+    void util(string s , int i , vector<string> temp){
+        int n = s.size();
+        if(i == n){
+            ans.push_back(temp);
+        }
+        string val = "";
+        for(int k = i; k<n; k++){
+           val += s[k];
+            if(pal(val)){
+                temp.push_back(val);
+                util(s , k+1 , temp);
+                temp.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<string> temp;
+        util(s , 0  , temp);
+        return ans;
+    }
+};
