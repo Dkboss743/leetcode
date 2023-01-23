418dd16c2bc53a7647859af7ca5325d5776a322f
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2023-01-23 14:27:53 +0530

    Time: 241 ms (43.43%), Space: 69 MB (29.65%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -4,10 +4,8 @@ public:
     int findJudge(int n, vector<vector<int>>& trust) {
         vector<int> gr(n+1 , 0);
         for(auto x : trust){
-            int a = x[0];
-            int b = x[1];
-            gr[b]++;
-            gr[a]--;
+            gr[x[1]]++;
+            gr[x[0]]--;
         }
         int ans = -1;
         for(int i = 1; i<=n; i++){
