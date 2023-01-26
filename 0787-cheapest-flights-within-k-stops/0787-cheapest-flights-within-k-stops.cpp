7518e8d00468cdfc8df24b90b57c5c0646fdcf8f
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2023-01-26 14:52:29 +0530

    Time: 37 ms (52.40%), Space: 13.2 MB (68.13%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    int dfs(vector<vector<pair<int,int>>> &gr, int src , int dst , int k,vector<vector<int>> &dp){
+    vector<vector<int>> dp;
+    vector<vector<pair<int,int>>> gr;
+    int dfs(int src , int dst , int k){
         if(src == dst){
             return 0;
         }
@@ -12,7 +14,7 @@ public:
         }
         int temp = INT_MAX;
         for(int i = 0; i<gr[src].size() ; i++){
-           int val = dfs(gr , gr[src][i].first , dst , k-1, dp);
+           int val = dfs(gr[src][i].first , dst , k-1);
             if(val != INT_MAX){
                 temp = min(temp , val+gr[src][i].second);
             }
@@ -21,12 +23,12 @@ public:
     }
     
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int,int>>> gr(n); 
+        gr.resize(n); 
         for(int i = 0 ; i<flights.size(); i++){
            gr[flights[i][0]].push_back({flights[i][1] , flights[i][2]});
         }
-        vector<vector<int>> dp(n+1, vector<int>(k+2 , -1));
-        int ans = dfs(gr ,src , dst , k+1, dp);
+        dp = vector<vector<int>>(n+1, vector<int>(k+2 , -1));
+        int ans = dfs(src , dst , k+1);
         return ans == INT_MAX ? -1 : ans;
         
     }
