42cbdd0d806c0cf2920aa65685856d5d5cf20217
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2023-01-26 14:53:41 +0530

    Time: 78 ms (18.62%), Space: 13.3 MB (68.13%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     vector<vector<int>> dp;
     vector<vector<pair<int,int>>> gr;
-    int dfs(int src , int dst , int k){
+    int dst;
+    int dfs(int src , int k){
         if(src == dst){
             return 0;
         }
@@ -14,7 +15,7 @@ public:
         }
         int temp = INT_MAX;
         for(int i = 0; i<gr[src].size() ; i++){
-           int val = dfs(gr[src][i].first , dst , k-1);
+           int val = dfs(gr[src][i].first , k-1);
             if(val != INT_MAX){
                 temp = min(temp , val+gr[src][i].second);
             }
@@ -27,8 +28,9 @@ public:
         for(int i = 0 ; i<flights.size(); i++){
            gr[flights[i][0]].push_back({flights[i][1] , flights[i][2]});
         }
+        this->dst = dst;
         dp = vector<vector<int>>(n+1, vector<int>(k+2 , -1));
-        int ans = dfs(src , dst , k+1);
+        int ans = dfs(src , k+1);
         return ans == INT_MAX ? -1 : ans;
         
     }
