a142244e2eab740c4ce3ca3865f04678eb89bed0
Divyash Kumar <66054595+Dkboss743@users.noreply.github.com>
2023-02-04 01:34:29 +0530

    Time: 40 ms (28.99%), Space: 15.8 MB (21.04%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -7,17 +7,12 @@ public:
         bool down = true;
         for(int i = 0 ; i<n; i++){
             mp[l] += s[i];
-            if(l == 0){
-                down = true;
-            }
-            else if(l == numRows-1){
-                down = false;
-            }
+            l == 0 ? down = true : l == numRows -1 ? down = false : down;
+           
             down ? l++ : l--; 
         }
         string ans = "";
         for(auto x : mp){
-            // cout << x.second << endl;
             ans += x.second;
         }
         return ans;
